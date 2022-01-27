b5993472aab4d8897dc0d67a17f60de0e6cb1207
Xiaoou Yang <hannahyxo@uga.edu>
2022-01-27 12:52:59 -0500

    TA

---
+++ lists.py
@@ -0,0 +1,5 @@
+a_list = [] #spcae is not required in py, but here = to delimit if = belongs to vatiable name or the latter
+print(type(a_list))
+
+a_list = [34,13,95]
+print(a_list[0]) #0 is the first 1
